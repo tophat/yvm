743ed7217bd2aeab55bf8f839bdbbd59358d2241
Brandon Baksh <brandon.baksh@tophatmonocle.com>
2018-06-18 11:23:14 -0400

    created arguement parser for command line

--- yvm.js
+++ yvm.js
@@ -1,1 +1,23 @@
+const argParser = require('commander')
+
 console.log('called yvm.js with args', process.argv)
+
+argParser
+    .command('install <version>')
+    .action(version => {
+        console.log(`Installing yarn v${version}`)
+    })
+
+argParser
+    .command('remove <version>')
+    .action(version => {
+        console.log(`Removing yarn v${version}`)
+    })
+
+argParser
+    .command('manage <version>')
+    .action(version => {
+        console.log(`Managing yarn v${version}`)
+    })
+
+argParser.parse(process.argv)
