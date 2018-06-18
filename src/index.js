7ab5071929ef5d8e21e21683a5458a368158e685
Brandon Baksh <brandon.baksh@tophatmonocle.com>
2018-06-18 15:20:26 -0400

    Addressed CR + npx pretty

--- src/index.js
+++ src/index.js
@@ -5,41 +5,36 @@ const request = require('request')
 const targz = require('targz')
 
 let directoryStack = []
+const yvmPath = path.resolve(os.homedir, '.yvm')
+const versionRootPath = path.resolve(yvmPath, 'versions')
 
 const checkDirectories = () => {
-    const homeDir = os.homedir();
-    const yvmPath = path.resolve(homeDir, './.yvm/')
-    const versionPath = path.resolve(yvmPath, './versions/')
-
-    if(!fs.existsSync(yvmPath)){
-        fs.mkdirSync(yvmPath)
-        directoryStack.push(yvmPath)
-    }
-    if(!fs.existsSync(versionPath)){
-        fs.mkdirSync(versionPath)
-        directoryStack.push(versionPath)
+    if (!fs.existsSync(versionRootPath)) {
+        fs.mkdirSync(versionRootPath)
+        directoryStack.push(versionRootPath)
     }
 }
 
 const cleanDirectories = () => {
-    while(directoryStack.length){
+    while (directoryStack.length) {
         fs.rmdirSync(directoryStack.pop())
     }
 }
 
 const checkForVersion = version => {
+    const versionPath = getVersionPath(version)
     checkDirectories()
-    return fs.existsSync(`~/.yvm/version/v${version}/`)
+    return fs.existsSync(versionPath)
 }
 
 const downloadVersion = version => {
-    const url = `https://yarnpkg.com/downloads/${version}/yarn-v${version}.tar.gz`
-    const versionPath = path.resolve(os.homedir(), `./.yvm/versions/v${version}/`)
-    if(!fs.existsSync(versionPath)){
+    const url = getUrl(version)
+    const versionPath = getVersionPath(version)
+    if (!fs.existsSync(versionPath)) {
         fs.mkdirSync(versionPath)
         directoryStack.push(versionPath)
     }
-    const filePath = path.resolve(os.homedir(), `./.yvm/versions/v${version}/yarn-v${version}.tar.gz`)
+    const filePath = getFilePath(version)
     const file = fs.createWriteStream(filePath)
 
     return new Promise((resolve, reject) => {
@@ -51,32 +46,43 @@ const downloadVersion = version => {
     })
 }
 
-const extractYarn = version =>{
-    const destPath = path.resolve(os.homedir(), `./.yvm/versions/v${version}`)
-    const srcPath = path.resolve(destPath, `./yarn-v${version}.tar.gz`)
-    targz.decompress({
-        src: srcPath,
-        dest: destPath
-    }, err => {
-        if (err){
-            console.log(err)
-        } else {
-            console.log(`Finished extracting yarn version ${version}`)
-            fs.unlinkSync(srcPath)
+const extractYarn = version => {
+    const destPath = getVersionPath(version)
+    const srcPath = getFilePath(version)
+    targz.decompress(
+        {
+            src: srcPath,
+            dest: destPath
+        },
+        err => {
+            if (err) {
+                console.log(err)
+            } else {
+                console.log(`Finished extracting yarn version ${version}`)
+                fs.unlinkSync(srcPath)
+            }
         }
-    })
+    )
 }
 
+const getFilePath = version =>
+    path.resolve(getVersionPath(version), `yarn-v${version}.tar.gz`)
+
+const getUrl = version =>
+    `https://yarnpkg.com/downloads/${version}/yarn-v${version}.tar.gz`
+
+const getVersionPath = version => path.resolve(versionRootPath, `v${version}`)
+
 const installVersion = version => {
-    if(checkForVersion(version)){
+    if (checkForVersion(version)) {
         return
     }
     downloadVersion(version)
-        .then(()=>{
+        .then(() => {
             console.log(`Finished downloading yarn version ${version}`)
             extractYarn(version)
         })
-        .catch( err => {
+        .catch(err => {
             console.log(`Downloading yarn failed: \n ${err}`)
             cleanDirectories()
         })
