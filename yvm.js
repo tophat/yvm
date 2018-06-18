25bbd0dfdea534185ddced31ad78ec87e6ff40a0
brandonbaksh <39271619+brandonbaksh@users.noreply.github.com>
2018-06-18 15:58:02 -0400

    Issue/install yarn (#16)
    * created arguement parser for command line
    
    * download yarn version
    
    * extract and remove yarn.tar.gz
    
    * package.json with targz
    
    * Addressed CR + npx pretty
    
    * Updated package+CI
    
    * forgot to make something a function
    
    * renaming functions + renaming director
    
    * fixed spelling errors

--- yvm.js
+++ yvm.js
@@ -1,1 +1,26 @@
+const argParser = require('commander')
+const { installVersion } = require('./src/index.js')
+
 console.log('called yvm.js with args', process.argv)
+
+argParser
+    .command('install <version>')
+    .action(version => {
+        console.log(`Installing yarn v${version}`)
+        installVersion(version)
+    })
+
+argParser
+    .command('remove <version>')
+    .action(version => {
+        console.log(`Removing yarn v${version}`)
+
+    })
+
+argParser
+    .command('manage <version>')
+    .action(version => {
+        console.log(`Managing yarn v${version}`)
+    })
+
+argParser.parse(process.argv)
