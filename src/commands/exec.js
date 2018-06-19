3a7ff1dba6b9edb7a2028ca67db0bae8cb615dbe
brandonbaksh <39271619+brandonbaksh@users.noreply.github.com>
2018-06-19 11:54:00 -0400

    Yvm exec (#22)
    * Added 'basic' yarn exec command
    
    * clean up console logs
    
    * Addressed CR comments
    
    * fix lint
    
    * fix eslint disables

---
+++ src/commands/exec.js
@@ -0,0 +1,24 @@
+/* eslint-disable global-require,import/no-dynamic-require */
+const fs = require('fs')
+const os = require('os')
+const path = require('path')
+
+const getYarnPath = version =>
+    path.resolve(os.homedir(), `.yvm/versions/v${version}`)
+
+const runYarn = (version, extraArgs) => {
+    process.argv = ['', ''].concat(extraArgs) // first two arguments are filler args [node version, yarn version]
+    require(path.resolve(getYarnPath(version), 'bin/yarn.js'))
+}
+
+const execCommand = (version, extraArgs) => {
+    if (!fs.existsSync(getYarnPath(version))) {
+        console.log(
+            `Yarn v${version} not found. Please install it using: yvm install ${version}`,
+        )
+        return
+    }
+    runYarn(version, extraArgs)
+}
+
+module.exports = execCommand
