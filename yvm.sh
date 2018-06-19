ad276ac7f6bd94ec95e053c87e3273f3d20e97a9
Francois Campbell <francoiscampbell@users.noreply.github.com>
2018-06-19 11:10:23 -0400

    Installer script (#14)
    * basic
    
    * remove -x
    
    * test
    
    * source not found?
    
    * inline vars, link to executable
    
    * falsify
    
    * unzip
    
    * refactor
    
    * make to run locally
    
    * newlines
    
    * define var, fix path
    
    * install in ci
    
    * ugh circle
    
    * don't install in ci
    
    * make yvm sourceable or executable
    
    * add sourcing
    
    * add npm install

--- yvm.sh
+++ yvm.sh
@@ -1,14 +1,29 @@
 #!/bin/sh
 
-command=$1
+echo "running yvm script"
 
 yvm_use() {
 	version=$1
 	echo "called yvm use with version $version"
 }
 
-if [ "$command" = "use" ]; then
-	yvm_use $2
+yvm_() {
+    echo "running yvm_"
+    command=$1
+    if [ "$command" = "use" ]; then
+        yvm_use $2
+    else
+        node ~/.yvm/yvm.js $@
+    fi
+}
+
+if [ -n "$PS1" ]; then
+    echo "This shell is interactive, declaring yvm function only"
+    yvm() {
+        echo "running yvm function"
+        yvm_ $@
+    }
 else
-	node yvm.js $@
+    echo "This shell is not interactive, running yvm now"
+    yvm_ $@
 fi
