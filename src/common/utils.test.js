b2d15a0dc0840c40eabb558cf65ff30435819a69
Jake Bolam <jake.bolam@gmail.com>
2018-06-19 18:41:15 -0400

    stuff

--- src/common/utils.test.js
+++
@@ -1,8 +0,0 @@
-const { getExtractionPath } = require('./utils')
-
-describe(`utils`, () => {
-    it(`getExtractionPath`, () => {
-        const path = getExtractionPath()
-        expect(path).toMatchSnapshot()
-    })
-})
---
+++ src/test.test.js
@@ -0,0 +1,5 @@
+describe(`Test`, () => {
+    it(`Test`, () => {
+        expect(true).toBe(false)
+    })
+})
