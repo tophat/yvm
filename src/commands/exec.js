bb33fcb33c098e3c747dfaf83b0022f1a98e1fdd
Brandon Baksh <brandon.baksh@tophatmonocle.com>
2018-06-19 11:39:17 -0400

    fix lint

--- src/commands/exec.js
+++ src/commands/exec.js
@@ -1,19 +1,21 @@
+/* eslint-disable global-require,prettier/prettier,import/no-dynamic-require */
 const fs = require('fs')
 const os = require('os')
 const path = require('path')
 
-const getYarnPath = version => (
+const getYarnPath = version =>
     path.resolve(os.homedir(), `.yvm/versions/v${version}`)
-)
 
 const runYarn = (version, extraArgs) => {
-    process.argv = ['',''].concat(extraArgs) //first two arguments are filler args [node version, yarn version]
+    process.argv = ['', ''].concat(extraArgs) // first two arguments are filler args [node version, yarn version]
     require(path.resolve(getYarnPath(version), 'bin/yarn.js'))
 }
 
 const execCommand = (version, extraArgs) => {
-    if(!fs.existsSync(getYarnPath(version))) {
-        console.log(`Yarn v${version} not found. Please install it using: yvm install ${version}`)
+    if (!fs.existsSync(getYarnPath(version))) {
+        console.log(
+            `Yarn v${version} not found. Please install it using: yvm install ${version}`,
+        )
         return
     }
     runYarn(version, extraArgs)
