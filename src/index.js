0f0b1e850c628247e625620c406b611d60927446
Francois Campbell <francois.campbell@tophatmonocle.com>
2018-06-19 11:08:11 -0400

    don't prettify dispatch function

--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
-/* eslint-disable global-require */
+/* eslint-disable global-require,prettier/prettier */
 const argParser = require('commander')
 
-module.exports = function(args) {
-    argParser.command('install <version>').action(version => {
-        console.log(`Installing yarn v${version}`)
-        const install = require('./commands/install')
-        install(version)
-    })
+module.exports = function dispatch(args) {
+    argParser
+        .command('install <version>')
+        .action(version => {
+            console.log(`Installing yarn v${version}`)
+            const install = require('./commands/install')
+            install(version)
+        })
 
-    argParser.command('remove <version>').action(version => {
-        console.log(`Removing yarn v${version}`)
-    })
+    argParser
+        .command('remove <version>')
+        .action(version => {
+            console.log(`Removing yarn v${version}`)
+        })
 
-    argParser.command('manage <version>').action(version => {
-        console.log(`Managing yarn v${version}`)
-    })
+    argParser
+        .command('manage <version>')
+        .action(version => {
+            console.log(`Managing yarn v${version}`)
+        })
 
     argParser.parse(args)
 }
