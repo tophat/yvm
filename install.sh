8d1e55f9d838d4f4d107cee88e0a9483bd88a528
Francois Campbell <francois.campbell@tophatmonocle.com>
2018-06-19 10:50:38 -0400

    add sourcing

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ sh_install_path="${install_dir}/yvm.sh"
 js_install_path="${install_dir}/yvm.js"
 
 executable_alias_path="/usr/local/bin/yvm"
+executable_source_string="source ${executable_alias_path}"
 
 if [ "$use_local" = true ]; then
     rm ${executable_alias_path}
@@ -27,3 +28,8 @@ fi
 
 chmod +x ${sh_install_path}
 ln -s ${sh_install_path} ${executable_alias_path}
+
+if ! grep -q "${executable_source_string}" ~/.zshrc; then
+    echo '' >> ~/.zshrc
+    echo ${executable_source_string} >> ~/.zshrc
+fi
