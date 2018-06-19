734063050a49e57a87cfecd160a9230dbafcf2fc
Milan Milojic <milan.milojic@tophatmonocle.com>
2018-06-19 11:36:03 -0400

    WIP

--- yvm.sh
+++ yvm.sh
@@ -7,24 +7,26 @@ YVM_DIR="${HOME}/.yvm" #this can be exported globally during install?
 yvm_use() {
 	local PROVIDED_VERSION=$1
 	echo "called yvm use with version $PROVIDED_VERSION"
-	echo "path is $(yvm_version_path $PROVIDED_VERSION)"
 
-	#change path
+	if yvm_is_version_installed "$PROVIDED_VERSION"; then
+		#change path
+	    local YVM_VERSION_DIR
+	    YVM_VERSION_DIR="$(yvm_version_path "$PROVIDED_VERSION")"
+	    echo "YVM_VERSION_DIR: $YVM_VERSION_DIR"
+      	# Change current version
+		PATH="$(yvm_change_path "$PATH" "/bin" "$YVM_VERSION_DIR")"
+		#export PATH
 
+	else 
+      	yvm_err "N/A: version \"${PROVIDED_VERSION}\" is not yet installed."
+		yvm_err ""
+	    yvm_err "You need to run \"yvm install ${PROVIDED_VERSION}\" to install it before using it."
+	    return 1
+	fi
+}
 
-    local YVM_VERSION_DIR
-    YVM_VERSION_DIR="$(yvm_version_path "$PROVIDED_VERSION")"
-    echo "YVM_VERSION_DIR: $YVM_VERSION_DIR"
-    
-	      # Change current version
-	echo "current $PATH"
-	PATH="$(yvm_change_path "$PATH" "/bin" "$YVM_VERSION_DIR")"
-
-	#export PATH
-	echo "new $PATH"
-
-	
-
+yvm_is_version_installed() {
+  [ -n "${1-}" ] && [ -x "$(yvm_version_path "$1" 2> /dev/null)"/bin/yarn ]
 }
 
 yvm_version_path() {
@@ -33,7 +35,7 @@ yvm_version_path() {
   if [ -z "${VERSION}" ]; then
     yvm_err 'version is required'
     return 3
-  else yvm_echo "$(yvm_version_dir)/${VERSION}"
+  else yvm_echo "$(yvm_version_dir)/v${VERSION}"
   fi
 }
 
@@ -54,7 +56,7 @@ yvm_version_dir() {
   local YVM_WHICH_DIR
   YVM_WHICH_DIR="${1-}"
   if [ -z "${YVM_WHICH_DIR}" ]; then
-    yvm_echo "${YVM_DIR}/versions/yarn"
+    yvm_echo "${YVM_DIR}/versions"
   else
     yvm_err 'unknown version dir'
     return 3
@@ -91,9 +93,23 @@ yvm_change_path() {
 }
 
 
+yvm_() {
+    echo "running yvm_"
+    command=$1
+    if [ "$command" = "use" ]; then
+        yvm_use $2
+    else
+        node ~/.yvm/yvm.js $@
+    fi
+}
 
-if [ "$command" = "use" ]; then
-	yvm_use $2
+if [ -n "$PS1" ]; then
+    echo "This shell is interactive, declaring yvm function only"
+    yvm() {
+        echo "running yvm function"
+        yvm_ $@
+    }
 else
-	node yvm.js $@
+    echo "This shell is not interactive, running yvm now"
+    yvm_ $@
 fi
