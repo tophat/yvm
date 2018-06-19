ffcf85794d464020e9e782d1e33edc4a37224462
Brandon Baksh <brandon.baksh@tophatmonocle.com>
2018-06-19 16:26:18 -0400

    moved code around

--- src/commands/list.js
+++ src/commands/list.js
@@ -5,20 +5,21 @@ const { printVersions, versionRootPath } = require('../common/utils')
 const getYarnVersions = () => {
     const re = /^v(\d+\.)(\d+\.)(\d+)$/
     const validItems = []
+
+    if (!fs.existsSync(versionRootPath)) {
+        return validItems
+    }
+
     fs.readdirSync(versionRootPath).forEach(file => {
         if (re.test(file)) {
             validItems.push(file)
         }
     })
+
     return validItems
 }
 
 const listVersions = () => {
-    if (!fs.existsSync(versionRootPath)) {
-        console.log('You have no yarn versions installed.')
-        return
-    }
-
     const installedVersions = getYarnVersions()
     if (installedVersions.length) {
         printVersions(installedVersions, 'Installed yarn versions:')
