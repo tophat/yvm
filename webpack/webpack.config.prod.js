ce3bcefffb6150269714ebf712d0563624c9bd4a
Francois Campbell <francois.campbell@tophatmonocle.com>
2018-06-20 14:14:47 -0400

    revert deploy branch to master, get release name from changelog

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -5,20 +5,15 @@ const GithubReleasePlugin = require('./githubReleasePlugin')
 
 const zipfileName = 'yvm.zip'
 
-const now = new Date().toISOString()
-const releaseName = `test-release_name-${now}`
-const releaseOptions = {
-    auth: { token: process.env.GITHUB_TOKEN },
-    name: releaseName,
-    assets: [zipfileName],
-}
-
 baseConfig.mode = 'production'
 baseConfig.plugins.push(
     new ZipPlugin({
         filename: zipfileName,
     }),
-    new GithubReleasePlugin(releaseOptions),
+    new GithubReleasePlugin({
+        auth: { token: process.env.GITHUB_TOKEN },
+        assets: [zipfileName],
+    }),
 )
 
 module.exports = baseConfig
