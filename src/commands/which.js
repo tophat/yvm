f44187ca082f3bc89cdf009ca9d0eef46690a053
Milan Milojic <milan.milojic@tophatmonocle.com>
2018-06-21 15:46:31 -0400

    which command rewrite

--- src/commands/which.js
+++ src/commands/which.js
@@ -1,32 +1,42 @@
 const { getRcFileVersion } = require('../util/version')
 const log = require('../common/log')
+const { versionRootPath } = require('../common/utils')
 const shell = require('shelljs')
 
-const whichCommand = () => {
+const whichCommand = inputPath => {
     if (!shell.which('yarn')) {
         shell.echo('Sorry, yarn in NOT installed.')
         shell.exit(1)
     }
-    const regex = /(\/Users)(\/)((?:[a-z][a-z0-9_]*))(\/)(\.yvm)(\/versions)(\/v\d+\.?\d+\.?\d+)(\/bin)/gm
-    const versionRegex = /(v\d+\.?\d*\.?\d*)/gm
-    const matchedString = regex.exec(process.env.PATH)
-    if (matchedString !== null) {
-        const matchedPath = matchedString[0]
-        const matchedVersion = matchedPath.toString().match(versionRegex)
-        log(`matched yvm version: ${matchedVersion} in PATH ${matchedPath}`)
-
-        const pathVersion = matchedVersion.toString().replace(/v/g, '')
-        const rcVersion = getRcFileVersion()
-        if (rcVersion !== null) {
-            if (pathVersion === rcVersion) {
-                log(`your RC version matches your PATH version, good job!`)
-            } else {
-                log(`your RC and PATH versions don't match :(`)
-            }
-        }
-    } else {
-        log(`You don't have yvm version installed`)
+    const envPath = inputPath || process.env.PATH
+    if (envPath === null || envPath === '') {
+        process.exit(1)
     }
+
+    const pathVariables = envPath.split(':')
+    pathVariables.forEach(element => {
+        if (element.startsWith(versionRootPath)) {
+            const versionRegex = /(v\d+\.?\d*\.?\d*)/gm
+            const matchedVersion = element.match(versionRegex)
+            log(`matched yvm version: ${matchedVersion} in PATH ${element}`)
+
+            const pathVersion = matchedVersion.toString().replace(/v/g, '')
+            const rcVersion = getRcFileVersion()
+            if (rcVersion !== null) {
+                if (pathVersion === rcVersion) {
+                    log(`your RC version matches your PATH version, good job!`)
+                } else {
+                    log(
+                        `your RC version: ${rcVersion} and PATH version: ${pathVersion} don't match :(`,
+                    )
+                }
+            }
+            process.exit(0)
+        }
+    })
+
+    log(`You don't have yvm version installed`)
+    process.exit(1)
 }
 
 module.exports = whichCommand
