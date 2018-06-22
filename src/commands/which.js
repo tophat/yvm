5c7b20d61e5a5aa7268276f2493413a1d3ca0907
Milan Milojic <nepodmitljivi@gmail.com>
2018-06-22 11:16:26 -0400

    which command implementation (#47)
    * which command implemented with string manipulation
    
    * updated which command
    
    * lint fix
    
    * lint changes
    
    * Delete .yvmrc
    
    * added command description
    
    * which command rewrite
    
    * added tests
    
    * removed tests
    
    * added process.exit

---
+++ src/commands/which.js
@@ -0,0 +1,49 @@
+const { getRcFileVersion } = require('../util/version')
+const log = require('../common/log')
+const { yvmPath, versionRootPath } = require('../common/utils')
+const shell = require('shelljs')
+
+const whichCommand = inputPath => {
+    if (!shell.which('yarn')) {
+        shell.echo('Sorry, yarn in NOT installed.')
+        shell.exit(1)
+    }
+    const envPath = inputPath || process.env.PATH
+    if (envPath === null || envPath === '') {
+        return 1
+    }
+
+    let foundVersion = false
+
+    const pathVariables = envPath.split(':')
+    pathVariables.forEach(element => {
+        if (element.startsWith(versionRootPath(yvmPath))) {
+            const versionRegex = /(v\d+\.?\d*\.?\d*)/gm
+            const matchedVersion = element.match(versionRegex)
+            log(`matched yvm version: ${matchedVersion} in PATH ${element}`)
+
+            const pathVersion = matchedVersion.toString().replace(/v/g, '')
+            const rcVersion = getRcFileVersion()
+            if (rcVersion !== null) {
+                if (pathVersion === rcVersion) {
+                    log(`your RC version matches your PATH version, good job!`)
+                } else {
+                    log(
+                        `your RC version: ${rcVersion} and PATH version: ${pathVersion} don't match :(`,
+                    )
+                }
+            }
+            foundVersion = true
+        }
+        return 0
+    })
+
+    if (!foundVersion) {
+        log(`You don't have yvm version installed`)
+        return 2
+    }
+
+    return 0
+}
+
+module.exports = whichCommand
