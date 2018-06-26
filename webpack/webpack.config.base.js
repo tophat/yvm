1b5e7c121c5a0809a0566658cd3f51831b7c5786
Jake Bolam <jake.bolam@gmail.com>
2018-06-26 12:07:04 -0400

    Update webpack.config.base.js

--- webpack/webpack.config.base.js
+++ webpack/webpack.config.base.js
@@ -16,7 +16,6 @@ module.exports = {
         rules: [
             {
                 test: /\.js$/,
-                exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader',
                     options: {
