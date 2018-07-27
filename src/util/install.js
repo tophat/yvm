tophat/yvm

commit ca92b1e1c3aa65173a16e2e8437df766348f87b9

Move PATH editing logic to JS (#118)
## Description
<!-- Add a bulleted list of items changed or added -->
- Changes `yvm use` to use `yvm get-new-path` under the hood, moving most of the logic into JS for consistency
- Closes #112

src/yvm.js
@@ -7,12 +7,12 @@ const {
 } = require('./util/version')
 const log = require('./common/log')
 
-const withRcFileVersion = action => (maybeVersionArg, ...rest) => {
+// eslint-disable-next-line consistent-return
+const getYarnVersion = (maybeVersionArg, ...rest) => {
     if (maybeVersionArg) {
         if (isValidVersionString(maybeVersionArg)) {
             log.info(`Using provided version: ${maybeVersionArg}`)
-            action(maybeVersionArg, ...rest)
-            return
+            return [maybeVersionArg, ...rest]
         }
     }
 
@@ -22,12 +22,17 @@ const withRcFileVersion = action => (maybeVersionArg, ...rest) => {
         const version = getRcFileVersion()
         validateVersionString(version)
         log.info(`Using yarn version: ${version}`)
-        action(version, ...rest)
+        return [version, ...rest]
     } catch (e) {
         log(e.message)
         process.exit(1)
     }
 }
+
+const invalidCommandLog = () =>
+    log(
+        'You need to source yvm to use this command. run `source /usr/local/bin/yvm`',
+    )
 
 /* eslint-disable global-require,prettier/prettier */
 argParser
@@ -47,10 +52,11 @@ argParser
     .command('install [version]')
     .alias('i')
     .description('Install the specified version of Yarn.')
-    .action(withRcFileVersion(version => {
+    .action(maybeVersion => {
+        const [version] = getYarnVersion(maybeVersion)
         const install = require('./commands/install')
         install(version)
-    }))
+    })
 
 argParser
     .command('remove <version>')
@@ -68,7 +74,7 @@ argParser
 argParser
     .command('use [version]')
     .description('Activate specified Yarn version, or use .yvmrc if present.')
-    .action(() => log('You need to source yvm to use this command. run `source /usr/local/bin/yvm`'))
+    .action(invalidCommandLog)
 
 argParser
     .command('which')
@@ -99,9 +105,18 @@ argParser
     })
 
 argParser
+    .command('get-new-path [version]')
+    .description('Internal command: Gets a new PATH string for "yvm use"')
+    .action(maybeVersion => {
+        const [version] = getYarnVersion(maybeVersion)
+        const getNewPath = require('./commands/getNewPath')
+        log.capturable(getNewPath(version))
+    })
+
+argParser
     .command('update-self')
     .description('Updates yvm to the latest version')
-    .action(() => log('You need to source yvm to use this command. run `source /usr/local/bin/yvm`'))
+    .action(invalidCommandLog)
 
 argParser
     .command('help')

test/commands/getNewPath.test.js
@@ -1,0 +1,40 @@
+const path = require('path')
+
+const getNewPath = require('../../src/commands/getNewPath')
+
+describe('getNewPath', () => {
+    it('Returns a new PATH string with a yarn directory prepended', () => {
+        const mockVersion = '1.7.0'
+        const mockRootPath = '/some/path'
+        const mockSplitPath = ['abc', 'def']
+        const mockPathString = mockSplitPath.join(path.delimiter)
+
+        const expectedPathString = [
+            `${mockRootPath}/versions/v${mockVersion}/bin`,
+            ...mockSplitPath,
+        ].join(path.delimiter)
+        expect(getNewPath(mockVersion, mockRootPath, mockPathString)).toEqual(
+            expectedPathString,
+        )
+    })
+
+    it('Returns a new PATH string with they yarn dir edited if it was already present', () => {
+        const mockVersion = '1.7.0'
+        const mockRootPath = '/some/path'
+        const mockSplitPath = [
+            'abc',
+            `${mockRootPath}/versions/v1.6.0/bin`,
+            'def',
+        ]
+        const mockPathString = mockSplitPath.join(path.delimiter)
+
+        const expectedPathString = [
+            'abc',
+            `${mockRootPath}/versions/v${mockVersion}/bin`,
+            'def',
+        ].join(path.delimiter)
+        expect(getNewPath(mockVersion, mockRootPath, mockPathString)).toEqual(
+            expectedPathString,
+        )
+    })
+})

src/commands/getNewPath.js
@@ -1,0 +1,32 @@
+const path = require('path')
+
+const { versionRootPath, yvmPath } = require('../common/utils')
+
+const getNewPath = (
+    version,
+    rootPath = yvmPath,
+    pathString = process.env.PATH,
+) => {
+    const splitPath = pathString.split(path.delimiter)
+    const destPath = versionRootPath(rootPath)
+
+    const newPathSegment = `${destPath}/v${version}/bin`
+    let hadExistingPath = false
+
+    for (let i = 0; i < splitPath.length; i += 1) {
+        const pathSegment = splitPath[i]
+        if (pathSegment.startsWith(destPath)) {
+            splitPath[i] = newPathSegment
+            hadExistingPath = true
+            break
+        }
+    }
+
+    if (!hadExistingPath) {
+        splitPath.unshift(newPathSegment)
+    }
+
+    return splitPath.join(path.delimiter)
+}
+
+module.exports = getNewPath

src/yvm.sh
@@ -4,34 +4,12 @@ command=$1
 YVM_DIR=${YVM_DIR-"${HOME}/.yvm"}
 
 yvm_use() {
-    local PROVIDED_VERSION=${1-$(head -n 1 .yvmrc)}
-
-    if [ -z "${PROVIDED_VERSION}" ]; then
-        yvm_err 'version is required'
-        return 3
-    fi
-
-    if ! yvm_is_version_installed "$PROVIDED_VERSION"; then
-        yvm_ 'function' install ${PROVIDED_VERSION}
-    fi
-
-    local YVM_VERSION_DIR
-    YVM_VERSION_DIR="$(yvm_version_path "$PROVIDED_VERSION")"
-    PATH="$(yvm_change_path "$PATH" "/bin" "$YVM_VERSION_DIR")"
-    echo "Set yarn version to ${PROVIDED_VERSION}"
-}
-
-yvm_is_version_installed() {
-    [ -n "${1-}" ] && [ -x "$(yvm_version_path "$1" 2> /dev/null)"/bin/yarn ]
-}
-
-yvm_version_path() {
-    local VERSION
-    VERSION="${1-}"
-    if [ -z "${VERSION}" ]; then
-        yvm_err 'version is required'
-        return 3
-    else yvm_echo "$(yvm_version_dir)/v${VERSION}"
+    local PROVIDED_VERSION=${1}
+    NEW_PATH=$(yvm_call_node_script get-new-path ${PROVIDED_VERSION})
+    if [ -z "${NEW_PATH}" ]; then
+        yvm_err "Could not get new path from yvm"
+    else
+        PATH=${NEW_PATH}
     fi
 }
 
@@ -43,35 +21,9 @@ yvm_err() {
     >&2 yvm_echo "$@"
 }
 
-yvm_version_dir() {
-    local YVM_WHICH_DIR
-    YVM_WHICH_DIR="${1-}"
-    if [ -z "${YVM_WHICH_DIR}" ]; then
-        yvm_echo "${YVM_DIR}/versions"
-    else
-        yvm_err 'unknown version dir'
-        return 3
-    fi
-}
-
-yvm_grep() {
-    GREP_OPTIONS='' command grep "$@"
-}
-
-yvm_change_path() {
-    # if there’s no initial path, just return the supplementary path
-    if [ -z "${1-}" ]; then
-        yvm_echo "${3-}${2-}"
-    # if the initial path doesn’t contain an yvm path, prepend the supplementary
-    # path
-elif ! yvm_echo "${1-}" | yvm_grep -q "${YVM_DIR}/versions/[^/]*${2-}"; then
-    yvm_echo "${3-}${2-}:${1-}"
-    # use sed to replace the existing yvm path with the supplementary path. This
-    # preserves the order of the path.
-else
-    yvm_echo "${1-}" | command sed \
-    -e "s#${YVM_DIR}/versions/[^/]*${2-}[^:]*#${3-}${2-}#"
-fi
+yvm_call_node_script() {
+    # do not add anything that outputs stuff to stdout in function, its output is stored in a variable
+    node "${YVM_DIR}/yvm.js" $@
 }
 
 case "$-" in
@@ -93,7 +45,7 @@ yvm_() {
     elif [ "${command}" = "update-self" ]; then
         curl https://raw.githubusercontent.com/tophatmonocle/yvm/master/scripts/install.sh | YVM_INSTALL_DIR=${YVM_DIR} bash
     else
-        node "${YVM_DIR}/yvm.js" $@
+        yvm_call_node_script $@
     fi
 }
 

commit a20492fc935c7fcf6b2efa104e6c10df4c7b2625

Re-enable auto-install on `yvm use` (#125)
## Description
That functionality broke in #118

src/util/install.js
@@ -1,0 +1,16 @@
+const fs = require('fs')
+
+const { getExtractionPath, yvmPath } = require('../util/utils')
+const installVersion = require('../commands/install')
+
+const ensureVersionInstalled = (version, rootPath = yvmPath) => {
+    const yarnBinDir = getExtractionPath(version, rootPath)
+    if (fs.existsSync(yarnBinDir)) {
+        return Promise.resolve()
+    }
+    return installVersion(version, rootPath)
+}
+
+module.exports = {
+    ensureVersionInstalled,
+}

src/util/version.js
@@ -18,7 +18,7 @@ function getRcFileVersion() {
 function validateVersionString(versionString) {
     if (versionString === null) {
         throw new Error(
-            `No yarn version supplied!\nTry adding a config file (.yvmrc) or specify your version in the command like this:\nyvm exec 1.0.2 install`,
+            `No yarn version supplied!\nTry adding a config file (.yvmrc) or specify your version in the command like this: "yvm install 1.7.0"`,
         )
     }
     if (!isValidVersionString(versionString) && versionString !== null) {
@@ -26,8 +26,29 @@ function validateVersionString(versionString) {
     }
 }
 
+// eslint-disable-next-line consistent-return
+const getSplitVersionAndArgs = (maybeVersionArg, ...rest) => {
+    if (maybeVersionArg) {
+        if (isValidVersionString(maybeVersionArg)) {
+            log.info(`Using provided version: ${maybeVersionArg}`)
+            return [maybeVersionArg, rest]
+        }
+    }
+
+    rest.unshift(maybeVersionArg)
+
+    try {
+        const version = getRcFileVersion()
+        validateVersionString(version)
+        log.info(`Using yarn version: ${version}`)
+        return [version, rest]
+    } catch (e) {
+        log(e.message)
+        process.exit(1)
+    }
+}
+
 module.exports = {
-    isValidVersionString,
     getRcFileVersion,
-    validateVersionString,
+    getSplitVersionAndArgs,
 }

src/yvm-exec.js
@@ -1,15 +1,10 @@
 /* eslint-disable global-require,import/no-dynamic-require */
-const fs = require('fs')
 const path = require('path')
 
-const { yvmPath } = require('./util/utils')
-const {
-    getRcFileVersion,
-    isValidVersionString,
-    validateVersionString,
-} = require('./util/version')
+const { ensureVersionInstalled } = require('./util/install')
+const { getSplitVersionAndArgs } = require('./util/version')
 const log = require('./util/log')
-const installVersion = require('./commands/install')
+const { yvmPath } = require('./util/utils')
 
 const getYarnPath = (version, rootPath) =>
     path.resolve(rootPath, `versions/v${version}`)
@@ -29,47 +24,17 @@ const runYarn = (version, extraArgs, rootPath) => {
 }
 
 const execCommand = (version, extraArgs = ['-v'], rootPath = yvmPath) =>
-    new Promise(resolve => {
-        validateVersionString(version)
-        log.info(`Using yarn version: ${version}`)
-        const yarnBinDir = getYarnPath(version, rootPath)
-
-        if (!fs.existsSync(yarnBinDir)) {
-            return resolve(
-                installVersion(version, rootPath).then(() =>
-                    runYarn(version, extraArgs, rootPath),
-                ),
-            )
-        }
-
-        return resolve(runYarn(version, extraArgs, rootPath))
-    })
-
-const getVersionAndYarnFromProcessArgs = () =>
-    new Promise(resolve => {
-        const [, , maybeVersionArg, ...args] = process.argv
-        const versionArgValid = isValidVersionString(maybeVersionArg)
-        const rcVersion = getRcFileVersion()
-
-        const version = versionArgValid ? maybeVersionArg : rcVersion
-
-        if (!versionArgValid && maybeVersionArg !== undefined) {
-            args.unshift(maybeVersionArg)
-        }
-
-        resolve({
-            version,
-            args,
-        })
-    })
+    ensureVersionInstalled(version, rootPath).then(() =>
+        runYarn(version, extraArgs, rootPath),
+    )
 
 if (require.main === module) {
-    getVersionAndYarnFromProcessArgs()
-        .then(({ version, args }) => execCommand(version, args))
-        .catch(err => {
-            log(err.message)
-            process.exit(1)
-        })
+    const [, , maybeVersionArg, ...rest] = process.argv
+    const [version, args] = getSplitVersionAndArgs(maybeVersionArg, ...rest)
+    execCommand(version, args).catch(err => {
+        log(err.message)
+        process.exit(1)
+    })
 }
 
 module.exports = execCommand

src/yvm.js
@@ -1,33 +1,8 @@
 const argParser = require('commander')
 
-const {
-    getRcFileVersion,
-    isValidVersionString,
-    validateVersionString,
-} = require('./util/version')
+const { ensureVersionInstalled } = require('./util/install')
+const { getSplitVersionAndArgs } = require('./util/version')
 const log = require('./util/log')
-
-// eslint-disable-next-line consistent-return
-const getYarnVersion = (maybeVersionArg, ...rest) => {
-    if (maybeVersionArg) {
-        if (isValidVersionString(maybeVersionArg)) {
-            log.info(`Using provided version: ${maybeVersionArg}`)
-            return [maybeVersionArg, ...rest]
-        }
-    }
-
-    rest.unshift(maybeVersionArg)
-
-    try {
-        const version = getRcFileVersion()
-        validateVersionString(version)
-        log.info(`Using yarn version: ${version}`)
-        return [version, ...rest]
-    } catch (e) {
-        log(e.message)
-        process.exit(1)
-    }
-}
 
 const invalidCommandLog = () =>
     log(
@@ -53,7 +28,7 @@ argParser
     .alias('i')
     .description('Install the specified version of Yarn.')
     .action(maybeVersion => {
-        const [version] = getYarnVersion(maybeVersion)
+        const [version] = getSplitVersionAndArgs(maybeVersion)
         const install = require('./commands/install')
         install(version)
     })
@@ -106,11 +81,11 @@ argParser
 
 argParser
     .command('get-new-path [version]')
-    .description('Internal command: Gets a new PATH string for "yvm use"')
+    .description('Internal command: Gets a new PATH string for "yvm use", installing the version if necessary')
     .action(maybeVersion => {
-        const [version] = getYarnVersion(maybeVersion)
+        const [version] = getSplitVersionAndArgs(maybeVersion)
         const getNewPath = require('./commands/getNewPath')
-        log.capturable(getNewPath(version))
+        ensureVersionInstalled(version).then(() => log.capturable(getNewPath(version)))
     })
 
 argParser

test/commands/getNewPath.test.js
@@ -18,7 +18,7 @@ describe('getNewPath', () => {
         )
     })
 
-    it('Returns a new PATH string with they yarn dir edited if it was already present', () => {
+    it('Returns a new PATH string with the yarn dir edited if it was already present', () => {
         const mockVersion = '1.7.0'
         const mockRootPath = '/some/path'
         const mockSplitPath = [