71c291d53e8327296b2c37f6fffa6a46b45d66b9
Martin Lai <martin.lai@tophatmonocle.com>
2018-10-17 14:44:46 -0400

    does not rely on interactive mode in fish shell

--- src/yvm.fish
+++ src/yvm.fish
@@ -2,15 +2,15 @@
 
 function yvm
     set command $argv[1]
-    set -q YVM_DIR; or set YVM_DIR "$HOME/.yvm"
+    set -q YVM_DIR; or set -U YVM_DIR "$HOME/.yvm"
 
     function yvm_use
         set PROVIDED_VERSION $argv[1]
-        set NEW_PATH (yvm_call_node_script get-new-path $PROVIDED_VERSION)
+        set NEW_PATH (yvm_call_node_script get-new-path $PROVIDED_VERSION | tr ':' \n)
         if [ -z "$NEW_PATH" ]
             yvm_err "Could not get new path from yvm"
         else
-            set -U PATH $NEW_PATH
+            set -Ux PATH $NEW_PATH
             yvm_echo "Now using yarn version (yarn --version)"
         end
     end
@@ -28,37 +28,11 @@ function yvm
         node "$YVM_DIR/yvm.js" $argv
     end
 
-    set interactive 0
-    for option in $argv
-        switch "$option"
-            case -i
-                set interactive 1
-            case \*
-        end
-    end
-
-    function yvm_
-        set mode $argv[1]
-        set command $argv[2]
-
-        if [ "$command" = "use" ]
-            if [ "$mode" = 'script' ]
-                yvm_err '"yvm use" can only be used when yvm is a shell function, not a script. Did you forget to source yvm?'
-                # exit 1
-            end
-            yvm_use $argv[3]
-        else if [ "$command" = "update-self" ]
-            curl -fsSL https://raw.githubusercontent.com/tophat/yvm/master/scripts/install.sh | YVM_INSTALL_DIR=${YVM_DIR} bash
-        else
-            yvm_call_node_script $argv[2..-1]
-        end
-    end
-
-    if [ $interactive = 1 ]
-        function yvm
-            yvm_ 'function' $argv[1..-1]
-        end
+    if [ "$command" = "use" ]
+        yvm_use $argv[2]
+    else if [ "$command" = "update-self" ]
+        curl -fsSL https://raw.githubusercontent.com/tophat/yvm/master/scripts/install.sh | YVM_INSTALL_DIR=${YVM_DIR} bash
     else
-        yvm_ 'script' $argv[1..-1]
+        yvm_call_node_script $argv[1..-1]
     end
 end
