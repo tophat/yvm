361a85b35490ab579b4e21d250fc829f893e2578
Aser Eldamaty <aeldamaty@users.noreply.github.com>
2018-11-21 09:50:10 -0500

    Automatically use the specified version of yarn after installing (#142)
    * Automatically use the specified version of yarn after installing
    
    * Automatically select yarn when opening shell
    
    * Fix automatically using yarn, checking for node, and tiny performance improvements
    
    * Check if file exists before editing last used version

--- src/yvm.sh
+++ src/yvm.sh
@@ -2,6 +2,34 @@
 
 command=$1
 YVM_DIR=${YVM_DIR-"${HOME}/.yvm"}
+export_yvm_dir_string="export YVM_DIR=${YVM_DIR}"
+
+save_last_used_yarn_version() {
+    yarn_version="${1}"
+    if [ -z "$yarn_version" ]; then
+        yarn_version="$(yarn --version)"
+    fi
+
+    if [ -z "$yarn_version" ]; then
+        return
+    fi
+    
+    if [ -e ~/.zshrc ]; then
+        if ! grep -q "LAST_USED_YARN_VERSION" ~/.zshrc; then
+            sed -i '' 's/.*YVM_DIR.*/export LAST_USED_YARN_VERSION='"${yarn_version}"'\'$'\n&/g' ~/.zshrc
+        else
+            sed -i '' "s/LAST_USED_YARN_VERSION.*/LAST_USED_YARN_VERSION=${yarn_version}/" ~/.zshrc
+        fi
+    fi
+
+    if [ -e ~/.bash_profile ]; then
+        if ! grep -q "LAST_USED_YARN_VERSION" ~/.bash_profile; then
+            sed -i '' 's/.*YVM_DIR.*/export LAST_USED_YARN_VERSION='"${yarn_version}"'\'$'\n&/g' ~/.bash_profile
+        else
+            sed -i '' "s/LAST_USED_YARN_VERSION.*/LAST_USED_YARN_VERSION=${yarn_version}/" ~/.bash_profile
+        fi
+    fi
+}
 
 yvm_use() {
     local PROVIDED_VERSION=${1}
@@ -10,7 +38,12 @@ yvm_use() {
         yvm_err "Could not get new path from yvm"
     else
         PATH=${NEW_PATH}
+        YARN_VERSION="$(yarn --version)"
+        CURRENT_GLOBAL_YARN_VERSION="${LAST_USED_YARN_VERSION:-}"
         yvm_echo "Now using yarn version $(yarn --version)"
+        if [ ! -e .yvmrc ] && [ "$YARN_VERSION" != "$CURRENT_GLOBAL_YARN_VERSION" ]; then
+            save_last_used_yarn_version ${1}
+        fi
     fi
 }
 
@@ -47,6 +80,9 @@ yvm_() {
         curl -fsSL https://raw.githubusercontent.com/tophat/yvm/master/scripts/install.sh | YVM_INSTALL_DIR=${YVM_DIR} bash
     else
         yvm_call_node_script $@
+        if [ "${command}" = "install" ]; then
+            yvm_use $2
+        fi
     fi
 }
 
@@ -54,6 +90,17 @@ if [ ${interactive} = 1 ]; then
     yvm() {
         yvm_ 'function' $@
     }
+    last_used_yarn="${LAST_USED_YARN_VERSION:-}"
+    if [ -z "$last_used_yarn" ]; then
+        yvm_echo "No default yarn version set. Use yvm install <version> or yvm use <version> to fix this"
+    else
+        if ! type "node" > /dev/null; then
+            yvm_echo "YVM Could not automatically set yarn version."
+            yvm_echo "Please ensure your YVM env variables and sourcing are set below sourcing node/nvm in your .zshrc or .bash_profile"
+        else
+            yvm_use ${last_used_yarn}
+        fi
+    fi
 else
     yvm_ 'script' $@
 fi
