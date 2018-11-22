d4b9e7dfcd39995e898a5e8512a79c57c3d84233
Michael Rose <michael.rose@tophatmonocle.com>
2018-11-22 15:54:52 -0500

    Use shared eslint config

--- test/commands/which.test.js
+++ test/commands/which.test.js
@@ -1,6 +1,6 @@
 const which = require('../../src/commands/which')
 
-describe.only('yvm which command', () => {
+describe('yvm which command', () => {
     it('fails to find yarn version', () => {
         const path = '/Users/tophat/.nvm/versions/node/v6.11.5/bin:'
         expect(which(path)).toBe(2)
