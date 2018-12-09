78b28e5a22026f1afffe671a88f5216d9de1e412
Jake Bolam <jake.bolam@gmail.com>
2018-12-09 18:46:11 -0500

    test files

---
+++ test/sanity-test.sh
@@ -0,0 +1,51 @@
+#!/bin/bash -e
+
+GREEN='\033[0;32m'
+RED='\031[0;32m'
+NC='\033[0m' # No Color
+
+source ~/.yvm/yvm.sh
+
+testing() {
+    echo
+    echo
+    echo -e "${GREEN}>>> ${1}${NC}"
+}
+
+pass() {
+    echo -e "${GREEN} PASSED! ${NC}"
+}
+
+fail() {
+    echo -e "${RED} TEST FAILED! ${NC}"
+    echo "Test output::"
+    echo ${1}
+    exit 1
+}
+
+testing "yvm exec version command"
+yvm install 1.11.0
+test1_output=$(yvm exec 1.11.0 --version)
+if [[ ${test1_output} == "1.11.0" ]]; then
+    pass
+else
+    fail ${test1_output}
+fi
+
+testing "yvm use"
+yvm install 1.7.0
+yvm_use # in place of yvm use
+test2_output=$(yvm exec --version)
+if [[ ${test2_output} == "1.7.0" ]]; then
+    pass
+else
+    fail ${test2_output}
+fi
+
+testing "yvm use set yarn"
+test3_output=$(yarn --version)
+if [[ ${test3_output} == "1.7.0" ]]; then
+    pass
+else
+    fail ${test3_output}
+fi
