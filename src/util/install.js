5b86644b71ebe912112ddd4509bd52e5c927a0bd
Jake Bolam <jake.bolam@gmail.com>
2018-12-23 18:09:08 +0800

    bugfix: fix crash no method s exists (#268)
    * bugfix: fix crash no method s exists
    
    * Update install.js

--- src/util/install.js
+++ src/util/install.js
@@ -3,14 +3,14 @@ const fs = require('fs')
 const { getExtractionPath } = require('../util/utils')
 const { yvmPath } = require('../util/path')
 
-const installVersion = require('../commands/install')
+const { install } = require('../commands/install')
 
 const ensureVersionInstalled = (version, rootPath = yvmPath) => {
     const yarnBinDir = getExtractionPath(version, rootPath)
     if (fs.existsSync(yarnBinDir)) {
         return Promise.resolve()
     }
-    return installVersion(version, rootPath)
+    return install(version, rootPath)
 }
 
 module.exports = {
