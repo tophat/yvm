cbfc18b71997c30d1b18b2017c28a2ccf8dab0d9
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2019-01-22 20:40:52 -0500

    test: print versions behaviour

--- test/util/utils.test.js
+++ test/util/utils.test.js
@@ -1,3 +1,4 @@
+const log = require('../../src/util/log')
 const {
     DEFAULT_VERSION_TEXT,
     VERSION_IN_USE_SYMBOL,
@@ -5,35 +6,60 @@ const {
     printVersions,
 } = require('../../src/util/version')
 
+jest.mock('../../src/util/log')
+
 const versionInUse = '1.2.0'
 const defaultVersion = '1.3.0'
 const localVersions = ['1.1.0', versionInUse, defaultVersion]
 const versions = [...localVersions, '1.4.0', '1.5.0']
 
 describe('Util functions', () => {
-    it('Returns same number of versions passed to printVersion function', () => {
-        const versionsObject = printVersions({ list: versions, versionInUse })
+    afterEach(() => {
+        jest.resetAllMocks()
+    })
+
+    const verifyPrintedVersions = () => {
+        expect(log.mock.calls).toMatchSnapshot()
+        expect(log.notice.mock.calls).toMatchSnapshot()
+        expect(log.success.mock.calls).toMatchSnapshot()
+    }
+
+    it('Prints all versions passed to printVersion function', () => {
+        const versionsObject = printVersions({
+            message: 'print all versions',
+            list: versions,
+            versionInUse,
+        })
         expect(Object.keys(versionsObject)).toHaveLength(versions.length)
+        verifyPrintedVersions()
     })
 
     it('Highlights the version currently in use', () => {
-        const versionsObject = printVersions({ list: versions, versionInUse })
-        expect(versionsObject[versionInUse]).toBeDefined()
+        const versionsObject = printVersions({
+            message: 'highlight current version',
+            list: versions,
+            versionInUse,
+        })
+        expect(versionsObject[versionInUse]).toContain(versionInUse)
         expect(versionsObject[versionInUse]).toContain(VERSION_IN_USE_SYMBOL)
+        verifyPrintedVersions()
     })
 
     it('Highlights the default version', () => {
         const versionsObject = printVersions({
+            message: 'highlight default version',
             list: versions,
             versionInUse,
             defaultVersion,
         })
-        expect(versionsObject[defaultVersion]).toBeDefined()
+        expect(versionsObject[defaultVersion]).toContain(defaultVersion)
         expect(versionsObject[defaultVersion]).toContain(DEFAULT_VERSION_TEXT)
+        verifyPrintedVersions()
     })
 
     it('Highlights all installed versions', () => {
         const versionsObject = printVersions({
+            message: 'highlight installed versions',
             list: versions,
             versionInUse,
             defaultVersion,
@@ -42,8 +68,9 @@ describe('Util functions', () => {
         localVersions
             .filter(v => v !== versionInUse)
             .forEach(v => {
-                expect(versionsObject[v]).toBeDefined()
+                expect(versionsObject[v]).toContain(v)
                 expect(versionsObject[v]).toContain(VERSION_INSTALLED_SYMBOL)
             })
+        verifyPrintedVersions()
     })
 })
