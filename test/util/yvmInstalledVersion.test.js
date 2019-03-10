3f8c251187eb8e3c3ff84051ffd1328a5590d0fd
Emmanuel Ogbizi <iamogbz+github@gmail.com>
2019-03-10 13:08:39 -0400

    fix: do not show yvm version error (#320)

--- test/util/yvmInstalledVersion.test.js
+++ test/util/yvmInstalledVersion.test.js
@@ -1,9 +1,14 @@
 const mockFS = require('mock-fs')
+const log = require('../../src/util/log')
 const { yvmInstalledVersion } = require('../../src/util/yvmInstalledVersion')
 
 describe('yvm installed version', () => {
     const mockYVMDir = '/mock-yvm-root-dir'
+    jest.spyOn(log, 'default')
+
+    beforeEach(jest.clearAllMocks)
     afterEach(mockFS.restore)
+    afterAll(jest.restoreAllMocks)
 
     it('Finds version if installed', () => {
         const mockVersion = 'v1.2.3'
@@ -16,10 +21,13 @@ describe('yvm installed version', () => {
     })
 
     it('fails to find version if no file', () => {
+        delete process.env.YVM_VERBOSE
         expect(yvmInstalledVersion(mockYVMDir)).toBeUndefined()
+        expect(log.default).not.toHaveBeenCalled()
     })
 
     it('fails to find version if file does not contain valid json', () => {
+        delete process.env.YVM_VERBOSE
         const mockVersion = 'v1.2.3'
         mockFS({
             [mockYVMDir]: {
@@ -27,9 +35,11 @@ describe('yvm installed version', () => {
             },
         })
         expect(yvmInstalledVersion(mockYVMDir)).toBeUndefined()
+        expect(log.default).not.toHaveBeenCalled()
     })
 
     it('fails to find version if file does not valid json with "version" key', () => {
+        delete process.env.YVM_VERBOSE
         const mockVersion = 'v1.2.3'
         mockFS({
             [mockYVMDir]: {
@@ -37,5 +47,14 @@ describe('yvm installed version', () => {
             },
         })
         expect(yvmInstalledVersion(mockYVMDir)).toBeUndefined()
+        expect(log.default).not.toHaveBeenCalled()
+    })
+
+    it('logs reason for failure if verbose is true', () => {
+        process.env.YVM_VERBOSE = true
+        expect(yvmInstalledVersion(mockYVMDir)).toBeUndefined()
+        expect(log.default).toHaveBeenCalledTimes(1)
+        expect(log.default.mock.calls[0]).toMatchSnapshot()
+        delete process.env.YVM_VERBOSE
     })
 })
