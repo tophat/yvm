6a6fc61d5a4193ca5bcb0a9b9818eb061587771c
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2019-04-06 09:05:26 -0400

    test: unalias command

---
+++ test/commands/unalias.test.js
@@ -0,0 +1,37 @@
+import * as alias from '../../src/util/alias'
+import log from '../../src/util/log'
+import { unalias } from '../../src/commands/unalias'
+
+describe('unalias', () => {
+    jest.spyOn(log, 'default')
+    const unsetAlias = jest.spyOn(alias, 'unsetAlias')
+
+    beforeEach(jest.clearAllMocks)
+    afterAll(jest.restoreAllMocks)
+
+    it('handles deleted aliases', async () => {
+        unsetAlias.mockResolvedValueOnce(true)
+        const args = { name: 'mock-name', force: false, recursive: true }
+        expect(await unalias(args)).toBe(0)
+        expect(unsetAlias).toHaveBeenCalledWith(args)
+        expect(log.default).toHaveBeenCalledWith(
+            expect.stringContaining('successfully deleted'),
+        )
+    })
+
+    it('handles no deleted aliases', async () => {
+        unsetAlias.mockResolvedValueOnce(false)
+        const args = { name: 'mock-name', force: true, recursive: false }
+        expect(await unalias(args)).toBe(1)
+        expect(unsetAlias).toHaveBeenCalledWith(args)
+        expect(log.default).not.toHaveBeenCalled()
+    })
+
+    it('handles unset error', async () => {
+        unsetAlias.mockRejectedValueOnce(new Error('some message'))
+        const args = { name: 'mock-name' }
+        expect(await unalias(args)).toBe(2)
+        expect(unsetAlias).toHaveBeenCalledWith(args)
+        expect(log.default).toHaveBeenCalledWith('some message')
+    })
+})
