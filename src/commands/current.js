a4dc5f89e154839617d834ead9640d84fce1a02c
Emmanuel Ogbizi <emmanuel.ogbizi-ugbe@tophatmonocle.com>
2019-05-16 22:35:39 -0400

    feat: update current command output

--- src/commands/current.js
+++ src/commands/current.js
@@ -1,20 +1,24 @@
-import { getRcFileVersion, getVersionInUse } from 'util/version'
 import log from 'util/log'
 import { getYarnPathEntries, isYvmPath } from 'util/path'
+import { NOT_AVAILABLE, SYSTEM } from 'util/alias'
+import { getRcFileVersion, getVersionInUse } from 'util/version'
 
 export const current = async ({ shell } = {}) => {
     const versionInUse = await getVersionInUse()
+    const [maybeYvmYarn] = getYarnPathEntries(shell)
+    log.info(`yarn: ${maybeYvmYarn || 'none'}`)
+    const isYvmYarn = isYvmPath(maybeYvmYarn)
+    const yarnVersion = isYvmYarn ? versionInUse : SYSTEM
+    log.capturable(yarnVersion || NOT_AVAILABLE)
+
     if (!versionInUse) {
         log('Sorry, yarn is NOT installed.')
         return 1
     }
-    const [maybeYvmYarn] = getYarnPathEntries(shell)
-    log.info(`yarn exec: ${maybeYvmYarn}`)
-    if (!isYvmPath(maybeYvmYarn)) {
+    if (!isYvmYarn) {
         log('Yarn was NOT installed by yvm')
         return 2
     }
-    log(`Currently on yarn version ${versionInUse}`)
     const rcVersion = getRcFileVersion()
     if (rcVersion !== null) {
         if (versionInUse === rcVersion) {
