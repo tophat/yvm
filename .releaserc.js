9f83adae4fb31675438c75ece01da2ceb79f3729
Emmanuel Ogbizi <iamogbz+github@gmail.com>
2019-07-28 23:52:53 -0400

    chore: auto changelog updates (#420)
    * chore: add changelog release step
    
    * chore: add git release step

--- .releaserc.js
+++ .releaserc.js
@@ -1,12 +1,17 @@
 module.exports = {
-    'plugins': [
+    plugins: [
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
-        ["@semantic-release/github", {
-            "assets": [
-                {"path": "artifacts/yvm.zip", "name": "yvm.zip"},
-                {"path": "artifacts/webpack_build/yvm.js", "name": "yvm.js"},
-            ]
-        }]
-    ]
+        '@semantic-release/changelog',
+        [
+            '@semantic-release/github',
+            {
+                assets: [
+                    { path: 'artifacts/yvm.zip', name: 'yvm.zip' },
+                    { path: 'artifacts/webpack_build/yvm.js', name: 'yvm.js' },
+                ],
+            },
+        ],
+        '@semantic-release/git',
+    ],
 }
